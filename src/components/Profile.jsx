a6119b1354687a730872a7bf50145a6672e8f2b5
serhii_iklenko <sergeyiklenko@gmail.com>
2021-02-12 21:25:33 +0200

    create first page

---
+++ src/components/Profile.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg" />
+      </div>
+      <div className="avatar">
+        <img src="https://avochka.ru/img/kartinka/1/enot_glass.jpg"></img>
+      </div>
+      <div>
+        my posts
+        <div>new post</div>
+        <div>
+          <div>post 1</div>
+          <div>post 2</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
